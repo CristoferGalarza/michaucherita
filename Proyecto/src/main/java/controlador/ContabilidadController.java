df54c3aa0004de63c0da55670963a1fccef559f6
karinarichavala <karina.arichavala@epn.edu.ec>
2024-08-10 18:11:17 -0500

    feat: controller structure and organization packages

--- Proyecto/src/main/java/controlador/ContabilidadController.java
+++ Proyecto/src/main/java/controlador/ContabilidadController.java
@@ -1,9 +1,65 @@
 package controlador;
 
-import jakarta.servlet.http.HttpServlet;
+import java.io.IOException;
 
-//Es necesario transformaslos a servlets
-public class ContabilidadController extends HttpServlet{
-	private static final long serialVersionUID = 1L;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+@WebServlet("/ContabilidadController")
+public class ContabilidadController extends HttpServlet {
 	
+	private static final long serialVersionUID = 1L;
+
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		
+	}
+
+	@Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+	 //1. Obtener los parámetros
+	 //2. Hablar con el modelo
+	 //3. LLamar a la vista
+ 
+		
+	}
+
+	private void verdasboard(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		//1. Obtener los parámetros
+		//2. Hablar con el modelo
+		//3. LLamar a la vista
+	 
+		
+	}
+	
+	private void verMovimientos(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		//1. Obtener los parámetros
+		 //2. Hablar con el modelo
+		 //3. LLamar a la vista
+		
+	}
+	
+	private void registrarIngreso(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		//1. Obtener los parámetros
+		//2. Hablar con el modelo
+		//3. LLamar a la vista
+	 
+	}
+	
+	private void verCuenta(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		//1. Obtener los parámetros
+		//2. Hablar con el modelo
+		//3. LLamar a la vista
+	 
+	}
+	
+	
+	
+	
+	
+	
+
 }
