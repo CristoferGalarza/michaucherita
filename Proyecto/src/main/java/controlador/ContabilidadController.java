ca3b94dcc1dc5d86cd6fad95fbf395e6781ad85d
Nyarlath <cristofer.galarza@epn.edu.ec>
2024-08-10 17:18:06 -0500

    Parte inicial del proyecto

---
+++ Proyecto/src/main/java/controlador/ContabilidadController.java
@@ -0,0 +1,9 @@
+package controlador;
+
+import jakarta.servlet.http.HttpServlet;
+
+//Es necesario transformaslos a servlets
+public class ContabilidadController extends HttpServlet{
+	private static final long serialVersionUID = 1L;
+	
+}
