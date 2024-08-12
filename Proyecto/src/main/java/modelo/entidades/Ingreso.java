98ddf094ad19d852cb39ae5308db09a75f9c1589
David98Guevara <70986947+David98Guevara@users.noreply.github.com>
2024-08-11 22:42:24 -0500

    Update Ingreso.java

--- Proyecto/src/main/java/modelo/entidades/Ingreso.java
+++ Proyecto/src/main/java/modelo/entidades/Ingreso.java
@@ -1,5 +1,61 @@
 package modelo.entidades;
 
-public class Ingreso {
+import java.io.Serializable;
+import java.sql.Date;
+import java.util.List;
 
+public class Ingreso implements Serializable{
+	private static final long serialVersionUID = 1L;
+	
+	private String concepto;
+	private Date fecha;
+	private Double monto;
+
+	
+	public Ingreso() {
+		
+	}
+	
+
+	public Ingreso(String concepto, Date fecha, Double monto) {
+		super();
+		this.concepto = concepto;
+		this.fecha = fecha;
+		this.monto = monto;
+	}
+
+
+	public String getConcepto() {
+		return concepto;
+	}
+
+	public void setConcepto(String concepto) {
+		this.concepto = concepto;
+	}
+
+	public Date getFecha() {
+		return fecha;
+	}
+
+	public void setFecha(Date fecha) {
+		this.fecha = fecha;
+	}
+
+	public Double getMonto() {
+		return monto;
+	}
+
+	public void setMonto(Double monto) {
+		this.monto = monto;
+	}
+	
+	/***************METODOS DEL NEGOCIO
+	 * @return *************/
+  
+	public void agregarIngreso(Double monto) {
+        this.monto += monto;
+    }
+	
+	
 }
+
